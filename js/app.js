c5702d2ef21c15cebca2e27a9cf87c3c47e9196c
nidhigaday <nidhi.gaday@gmail.com>
2015-04-27 05:41:24 -0400

    Completed game

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 // Enemies our player must avoid
-var Enemy = function(locY) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+
+// possible vlues of Y axis locations for bugs
+var valueOfY = [50, 130, 210];
+var counter = 0;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+var Enemy = function () {
     this.sprite = 'images/enemy-bug.png';
-    this.x = 20;         // setting the Enemy initial location
-    this.y = locY * 88;
-    this.speed = this.getspeed();          // setting the Enemy speed
-    return this;
+    this.x = 0;
+
+    // randomly selected Y location for each bug
+    this.y = valueOfY[Math.floor(Math.random() * 3)];
+
+    // for random speed of the bugs
+    this.speed = Math.floor(100 + (Math.random() * 200));
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -18,7 +24,24 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += (this.speed*dt);  // movement compatibility
+
+    // movement compatibility
+    this.x += this.speed * dt;
+
+    // if enemy moves out of the frame, following code changes enemy's speed and Y location (row)
+    if (this.x > 505) {
+        this.x = -101;
+        this.y = this.y + 83;
+        this.speed = Math.floor(100 + (Math.random() * 200));
+
+        // avoids enemy to allocate any Y location on grass rows
+        if (this.y > 226) {
+            this.y = 60;
+        }
+    }
+
+    // checks collision between player and enemy and increment counter
+    checkCollision(this, player);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -26,43 +49,40 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.getspeed = function() {
-    var random = Math.random() + 0.5;
-    if (Math.floor(random) < 1) {
-        this.speed = 2.5;
-    } else {
-        this.speed = 5;
-    }
-    return this.speed;
-};
-
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(locx, locy){
-    this.sprite = 'images/char-horn-girl.png';  //setting Player's image
-    // setting up player's initial location
-    this.x = locx;
-    this.y = locy;
-    // setting up movement
-    this.moveX = 0;
-    this.moveY = 0;
+
+// setting up player function properties
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 200;
+    this.y = 400;
     return this;
 };
 
-Player.prototype.update = function(){
+Player.prototype.update = function() {
      // moving player in the direction button is pressed
-        if (this.x + this.moveX > 0 && this.x + this.moveX < 500) {
-           this.x += this.moveX;
-        } else {
-           this.x = 0;
-        }
-        if (this.y + this.moveY > 0 && this.y + this.moveY < 500) {
-           this.y += this.moveY;
-        } else {
-           this.y = 0;
-        }
+    if (this.ctrlKey === 'left' && this.x !== 0) {
+        this.x = this.x - 100;
+    } else if (this.ctrlKey === 'right' && this.x != 400) {
+        this.x = this.x + 100;
+    } else if (this.ctrlKey === 'up') {
+        this.y = this.y - 83;
+    } else if (this.ctrlKey === 'down' && this.y != 400) {
+        this.y = this.y + 83;
+    }
+    // setting value of ctlKey to null will give player a discreet movement
+    // otherwise, player will continue to move in the one direction unless
+    // change it direction of movement
+    // player will never come to a stop
+    this.ctrlKey = null;
+
+    // reseting player location
+    if (this.y < 60) {
+        alert('You Won!\n' + '\n' + 'Resetting Game..');
+        player.reset();
+    }
 };
 
 // Draw the player on the screen, required method for game
@@ -70,43 +90,43 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(allowedKeys){
-    var initialX = x, initialY = y;
-    if(allowedKeys === '37')
-    {
-        initialX = initialX--;
-    }
-    else if (allowedKeys === '38')
-    {
-        initialY = initialY++;
-    }
-    else if (allowedKeys === '39')
-    {
-        initialX = initialX++;
-    }
-    else if (allowedKeys === '40')
-    {
-        initialY = initialY--;
-    }
+Player.prototype.handleInput = function(key){
+    this.ctrlKey = key;
 };
 
+// resetting Players location
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+var checkCollision = function(bug, you) {
+    // check for collision between enemy and player
+    // limited number of collisions allowed, else Game over
+    if (!(bug.y + 83 < you.y ||
+        bug.y > you.y + 83 ||
+        bug.x + 100 < you.x ||
+        bug.x > you.x + 100)) {
+            counter = counter + 1;
+            if(counter >= 5) {
+                alert('Game Over!');
+                counter = 0;
+            }
+            you.reset();
+        }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var totalEnemies = 5;
+var totalEnemies = 4;
 var allEnemies = [];
 
 for (var i = 0; i < totalEnemies; i++) {
-    allEnemies.push(new Enemy(i));
-}
-
-var player = new Player(200, 600);
-
-function gameStart(){
     allEnemies.push(new Enemy());
-    player = new Player();
 }
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
